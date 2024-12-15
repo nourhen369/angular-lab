6a1042dfd6c4946753fc4e9550badea9f22046dc
ines-el-bour <ines.elbour@insat.ucar.tn>
2024-12-16 00:28:05 +0100

    Form kemel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorChangerComponent } from './color-changer/color-changer.component';
+import { FormAuthComponent } from './form-auth/form-auth.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorChangerComponent
+    ColorChangerComponent,
+    FormAuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-auth/form-auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-auth',
+  templateUrl: './form-auth.component.html',
+  styleUrls: ['./form-auth.component.css']
+})
+export class FormAuthComponent {
+  login(formAuth: any): void{}
+
+}
